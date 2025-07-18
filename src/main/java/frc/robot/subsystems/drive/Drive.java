51c1893daa9ca90ea23e0fe9062e57dcce30c18a
liam <liam.b@skunkworks1983.com>
2025-07-17 19:01:53 -0700

    added smartdashbord stuff

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -44,6 +44,7 @@ import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -232,6 +233,9 @@ public class Drive extends SubsystemBase {
 
     // Update gyro alert
     gyroDisconnectedAlert.set(!gyroInputs.connected && SimConstants.currentMode != Mode.SIM);
+
+    SmartDashboard.putBoolean("right lidar", dualLidar.isLidarRightTripped.getAsBoolean());
+    SmartDashboard.putBoolean("left lidar", dualLidar.isLidarLeftTripped.getAsBoolean());
   }
 
   /**
