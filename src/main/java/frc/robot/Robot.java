66d2bfe62729a733638d883e28db2aa349bb925f
lucashetrick <4031995@g.highlineschools.org>
2025-10-09 20:20:18 -0700

    forgot about teleopinit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -150,6 +150,8 @@ public class Robot extends LoggedRobot {
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
+    new MoveFunnelToSetpoint(robotContainer.funnel, FunnelConstants.FUNNEL_POSITION_LOW_CONVERTED)
+        .schedule();
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
